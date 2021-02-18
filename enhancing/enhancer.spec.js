0447e35cc3d131c9eeac1b824ab6a13cbb8ce34c
extrude575757 <nicholas.a.lacapria@gmail.com>
2021-02-17 18:10:34 -0800

    Enhacement callback comments

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,67 +1,67 @@
 const enhancement = require('./enhancer.js');
 // test away!
 
-
-function drinkAll(callback, enhancer) {
-    if(!enhancer.durability ){
+/*    enhanceAll(cb,eObj)
+    Recursive Callback Function 
+        @ Callback - the enhancement function ie success
+        @ enhancer - The enhancement object ie returning from getItm
+*/
+function enhanceAll(callback, enhancer) {
+    // When Enhancer Object Does not exist
+    if(enhancer === null ){
         const durability = enhancer.durability
-    
-    if (durability >= 0) {
-      callback(enhancer);
-    }
+        // If not null & if statement is wrong
+            if (enhancer !== null) {
+            // Pinpoints a bug when true because durability should not have a value here
+            // Place the enhancer in the callback to init success 
+            callback(enhancer);
+            }
+            //Else we will Test for when null
     }else{
-        console.log('kickrocks')
+        console.log('Enhanceing As Normal')
     }
   }
   
-  describe('drinkAll', () => {
-    test('enhancements callback is called', () => {
+  describe('enhanceAll as describe', () => {
+//All The Enhancement callbacks
+    test('enhancements callback is called with success spyon mock func', () => {
 
-    //   const drink = enhancement.success;
-    //     // jest.fn
-    //   drinkAll(drink, gI);
-    //   expect(drink).toHaveBeenCalled();
-
- 
-
+        // place the enhancement obj in gI
         const gI =  enhancement.getItm
+        // The jest Mock enhancement function for success
         const successSpy = jest.spyOn(enhancement, 'success');
+        // Initiates the Mock enhancement success function with gI 
         const successInit = enhancement.success(gI);
-
-
-        drinkAll(successInit, gI);
-
-        expect(successSpy).toHaveBeenCalled();
-        // expect(isPlaying).toBe(true);
-        // expect(isPlaying).toHaveBeenCalled();
+        // Calls the Recurisve Bug finding Function similar to successInit
+        enhanceAll(successInit, gI);
+        // Expect jest Mock enhancment function for success to have been called
+        expect(successSpy).toHaveBeenCalled(); 
+        // Last mockRestore() jestfunction to run with successSpy Mock function
         successSpy.mockRestore();
 
-    });
+    }); // End Enhancements Call is called 
   
-    test('enhancements callback is called NOT CALLED', () => {
-        // const gI = enhancement.getItm
-    //     const drink = enhancement.success;
-    //   drinkAll(drink, gI);
-    //   expect(drink).not.toHaveBeenCalled();
-
-        
+    test('enhancements callback is  NOT CALLED for success spyon mock func', () => {
+ 
+      // place the enhancement obj in gI
       const gI =  enhancement.getItm
+      // The jest Mock enhancement function for success
       const successSpy = jest.spyOn(enhancement, 'success');
+      // Initiates the Mock enhancement success function with gI 
       const successInit = enhancement.success(gI);
-
-
-
-
+        // When successSpy is called
       if(successSpy){
-        drinkAll(successInit, gI);
+          // Calls the Recurisve Bug finding Function similar to successInit
+        enhanceAll(successInit, gI);
       }else{
+            // Now we know success spy was not called here & is false
         expect(successSpy).not.toHaveBeenCalled();
-        expect(successSpy).toBe(false);
-        // expect(isPlaying).toHaveBeenCalled();
+        expect(successSpy).toBe(false); 
+        // Last mockRestore() jestfunction to run with successSpy Mock function
         successSpy.mockRestore();
       }
 
-    });
-  });
+    });// End enhancements callback is  NOT CALLED 
+  });  // End Enhancer Describe
 
 
